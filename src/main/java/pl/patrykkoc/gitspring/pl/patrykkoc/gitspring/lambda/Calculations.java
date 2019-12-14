125400e7a5702f6b6f271d0a7a1ea8ea36e6acca
PatrykKoc <patryk.koc44@gmail.com>
2019-12-14 14:47:26 +0100

    [edit] - RestController : add comment

--- src/main/java/pl/patrykkoc/gitspring/pl/patrykkoc/gitspring/lambda/Calculations.java
+++ src/main/java/pl/patrykkoc/gitspring/pl/patrykkoc/gitspring/lambda/Calculations.java
@@ -2,7 +2,7 @@ package pl.patrykkoc.gitspring.pl.patrykkoc.gitspring.lambda;
 
 public interface Calculations {
 
-    int calc(int pierwsze,int b);
+   // int calc(int pierwsze,int b);
 
    String person(String imie,String nazwisko,int wiek);
 }
--- src/main/java/pl/patrykkoc/gitspring/pl/patrykkoc/gitspring/lambda/LambdaExample.java
+++ src/main/java/pl/patrykkoc/gitspring/pl/patrykkoc/gitspring/lambda/LambdaExample.java
@@ -10,14 +10,14 @@ public class LambdaExample {
       //  System.out.println(performCalculations(1, 2, (a, b) -> a+b));
         //mnożenie
     //    System.out.println(performCalculations(1,2,(a,b) -> a*b));
-       // System.out.println(writePerson("Patryk","Koc",11,(a,b,c) -> a+":"+b+" - wiek"+c));
+        System.out.println(writePerson("Patryk","Koc",11,(a,b,c) -> a+":"+b+" - wiek"+c));
     }
 //
-    private static int performCalculations(int first,
-                                            int second,
-                                            Calculations calculations){
-        return calculations.calc(first,second);
-    }
+//    private static int performCalculations(int first,
+//                                            int second,
+//                                            Calculations calculations){
+//        return calculations.calc(first,second);
+//    }
     private static String writePerson(String imie, String nazwisko, int wiek, Calculations calculations){
         return calculations.person(imie,nazwisko,wiek);
     }
