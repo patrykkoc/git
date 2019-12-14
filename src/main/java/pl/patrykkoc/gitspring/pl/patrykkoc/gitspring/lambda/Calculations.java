8a2d30c5e349a6812bd80a554aa0f655248f58ef
PatrykKoc <patryk.koc44@gmail.com>
2019-12-14 15:54:52 +0100

    [add] - StreamExample

--- src/main/java/pl/patrykkoc/gitspring/pl/patrykkoc/gitspring/lambda/Calculations.java
+++
@@ -1,8 +0,0 @@
-package pl.patrykkoc.gitspring.pl.patrykkoc.gitspring.lambda;
-
-public interface Calculations {
-
-   // int calc(int pierwsze,int b);
-
-   String person(String imie,String nazwisko,int wiek);
-}
