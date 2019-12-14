35ae50a9393283c95b6aad815a4f5fb708095dd8
PatrykKoc <patryk.koc44@gmail.com>
2019-12-14 13:01:18 +0100

    [edit] - RestController

--- src/main/java/pl/patrykkoc/gitspring/controller/rest/MyRestController.java
+++ src/main/java/pl/patrykkoc/gitspring/controller/rest/MyRestController.java
@@ -1,5 +1,15 @@
 package pl.patrykkoc.gitspring.controller.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyRestController {
+
+
+    @GetMapping("/name")
+    String myName(){
+        return "Patryk";
+    }
 
 }
