5802f36d7694670b5e36b13f53c8e0eff8f104d5
PatrykKoc <patryk.koc44@gmail.com>
2019-12-14 14:46:00 +0100

    [edit] - RestController : add comment

---
+++ src/main/java/pl/patrykkoc/gitspring/pl/patrykkoc/gitspring/lambda/Calculations.java
@@ -0,0 +1,8 @@
+package pl.patrykkoc.gitspring.pl.patrykkoc.gitspring.lambda;
+
+public interface Calculations {
+
+    int calc(int pierwsze,int b);
+
+   String person(String imie,String nazwisko,int wiek);
+}
