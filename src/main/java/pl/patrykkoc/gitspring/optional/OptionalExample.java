21e2ec551203b939bc3ad56bb1f270847943e5db
PatrykKoc <patryk.koc44@gmail.com>
2019-12-15 08:08:02 +0100

    [edit] - add code to optionalExample

--- src/main/java/pl/patrykkoc/gitspring/optional/OptionalExample.java
+++ src/main/java/pl/patrykkoc/gitspring/optional/OptionalExample.java
@@ -1,5 +1,27 @@
 package pl.patrykkoc.gitspring.optional;
 
+import java.util.Optional;
+
 public class OptionalExample {
 
+    public static void main(String[] args) {
+
+        // start sposób - bez Lambdy
+         Optional<String> maybeNick = findNick();
+         if(maybeNick.isPresent())
+             System.out.println(maybeNick.get());
+
+        // nowy sposób
+        maybeNick.ifPresent(
+                nick -> System.out.println("Mam nicka" + nick)
+        );
+    }
+
+
+    private static Optional<String> findNick(){
+        Optional<String> maybeNick = Optional.of("A");
+     //   return Optional.empty();
+return maybeNick;
+
+    }
 }
